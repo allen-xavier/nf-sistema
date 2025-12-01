7a921c1deca4004650f25f38320637675ee10b57
allen-xavier <60575502+allen-xavier@users.noreply.github.com>
2025-12-01 18:53:14 -0300

    atualizacao

--- src/db.js
+++ src/db.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST || 'postgres',
+    host: process.env.DB_HOST || 'pgvector',
     port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
     logging: false,
